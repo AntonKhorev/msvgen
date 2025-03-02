46e1fd82f1190039290c08593fdc982dfd0850e4
Anton Khorev <tony29@yandex.ru>
2025-03-02 19:49:17 +0300

    Finish diagram measurements

--- src/explanation.ts
+++ src/explanation.ts
@@ -13,34 +13,52 @@ export default class Explanation {
 
 	render(): void {
 		const sx=250
-		const sy=400
-		const padding=4.5
-		const leftPadding=24.5
+		const sy=350
+
+		const pad0=28.5
+		const pad1=4.5
+
 		const h=sy
 		const r=sx/2
 		const p=h-r
-		this.$figure.innerHTML=`<svg width="${f(sx+padding+leftPadding)}" height="${f(sy+2*padding)}" viewBox="${f(-sx/2-leftPadding)} ${f(-sx/2-padding)} ${f(sx+padding+leftPadding)} ${f(sy+2*padding)}">
+		const y=r**2/p
+		const x=Math.sqrt(r**2-y**2)
+
+		this.$figure.innerHTML=`<svg width="${f(sx+pad0+pad1)}" height="${f(sy+pad0+pad1)}" viewBox="${f(-sx/2-pad0)} ${f(-sx/2-pad0)} ${f(sx+pad0+pad1)} ${f(sy+pad0+pad1)}">
 			<defs>
 				<marker id="arrow-head" markerWidth="16" markerHeight="8" viewBox="-16 -4 16 8" orient="auto-start-reverse">
-					<path d="M-12,0 L-16,4 L0,0 L-16,-4 Z" fill="currentColor" stroke="none" />
+					<path d="M-12,0 L-16,3 L0,0 L-16,-3 Z" fill="currentColor" stroke="none" />
 				</marker>
 			</defs>
 			<g fill="none" stroke="currentColor">
 				<path d="${computeOutlinePathCommands(f,f,sy,r)}" stroke-width="2" />
 				<line x1="-8" x2="8" />
 				<line y1="-8" y2="8" />
-				<line x1="${f(-r-8)}" x2="${f(-r-8)}" y1="${f(-r)}" marker-start="url(#arrow-head)" marker-end="url(#arrow-head)" />
-				<line x1="${f(-r-8)}" x2="${f(-r-8)}" y2="${f(p)}" marker-start="url(#arrow-head)" marker-end="url(#arrow-head)" />
 				<g stroke-dasharray="2">
 					<circle r="${f(r)}" />
+					<line x1="${f(-r)}" x2="${f(-r)}" y1="${f(-r-16)}" />
+					<line x1="${f(r)}" x2="${f(r)}" y1="${f(-r-16)}" />
 					<line x1="${f(-r-16)}" y1="${f(-r)}" y2="${f(-r)}" />
-					<line x1="${f(-r-16)}" />
 					<line x1="${f(-r-16)}" y1="${f(p)}" y2="${f(p)}" />
+				</g>
+				<g marker-start="url(#arrow-head)" marker-end="url(#arrow-head)">
+					<line x1="${f(-r)}" x2="${f(r)}" y1="${f(-r-8)}" y2="${f(-r-8)}" />
+					<line x1="${f(-r-8)}" x2="${f(-r-8)}" y1="${f(-r)}" y2="${f(p)}" />
+					<line y1="${f(-r)}" />
+					<line y2="${f(p)}" />
+					<line x2="${f(x)}" y2="${f(y)}" />
+					<line x2="${f(x)}" />
+					<line x1="${f(x)}" x2="${f(x)}" y2="${f(y)}" />
 				</g>
 			</g>
 			<g fill="currentColor">
-				<text x="${f(-r-12)}" y="${f(-r/2)}" text-anchor="end" dominant-baseline="middle">r</text>
-				<text x="${f(-r-12)}" y="${f(p/2)}" text-anchor="end" dominant-baseline="middle">p</text>
+				<text y="${f(-r-12)}" text-anchor="middle">w</text>
+				<text x="${f(-r-12)}" y="${f((p-r)/2)}" text-anchor="end" dominant-baseline="middle">h</text>
+				<text x="-4" y="${f(-r/2)}" text-anchor="end" dominant-baseline="middle">r</text>
+				<text x="-4" y="${f(p/2)}" text-anchor="end" dominant-baseline="middle">p</text>
+				<text x="${f(x/2)}" y="${f(y/2)}" text-anchor="middle"dominant-baseline="text-before-edge">r</text>
+				<text x="${f(x/2)}" y="-4" text-anchor="middle">x</text>
+				<text x="${f(x+4)}" y="${f(y/2)}" dominant-baseline="middle">y</text>
 			</g>
 		</svg>`
 
